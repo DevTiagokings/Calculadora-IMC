9f85add232a9a8def8363d3ae5c03d7acc9eaadf
Tiago Gomes do Nascimento <133234836+DevTiagokings@users.noreply.github.com>
2024-01-30 15:55:30 -0300

    Finalização do projeto

--- script.js
+++ script.js
@@ -11,9 +11,24 @@ function imc () {
 
         const valorIMC = (peso / (altura * altura)).toFixed(1);
         
-        let classificacao = "";
+        let classificacao = '';
 
-        resultado.textContent= valorIMC;
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso';
+        } else if (valorIMC < 25) {
+            classificacao = 'com peso ideal. Parabéns!!!';
+        } else if (valorIMC < 30) {
+            classificacao = 'levemente acima do peso.';
+        } else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I';
+        } else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II';
+        } else {
+            classificacao = 'com obesidade grau III. Cuidado!!!'
+        }
+
+
+        resultado.textContent= `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
     }else {
         resultado.textContent = 'Preencha todos os campos!!!';
     }
